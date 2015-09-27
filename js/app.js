bb4370e666bdf207405577f1db3f42c707b83fe0
BogdanRusinka <rusinka.bogdann@mail.ru>
2015-09-27 15:06:47 +0300

    Add styles and new logic to flex layout

--- js/app.js
+++ js/app.js
@@ -1,24 +1,104 @@
 angular.module('playground', [])
 	.controller('playgroundController', ['$scope', function($scope){
 		$scope.mainblocks = [{
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		},{
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem. Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem. Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		}, {
-			text: 'Lorem ipsum dolor sit amet'
+			text: 'Lorem ipsum dolor sit amet',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		}, {
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posue'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posue',
+			shrink: '0',
+			grow: '1',
+			basis: '250px'
 		}, {
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.Lorem ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet, consectetur',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		}, {
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		},{
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin olor sit amet, consectetur'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin olor sit amet, consectetur',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		},{
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		}, {
-			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.Lorem ipsum dolor sit amet, consectetur'
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin posuere ligula sem.Lorem ipsum dolor sit amet, consectetur',
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
 		}];
+		$scope.header = { 
+			justifying: 'center'
+		};
+		$scope.globalStyles = true;
+		$scope.global = {
+			shrink: '0',
+			grow: '1',
+			basis: '250px',
+			order: '',
+			alignself: 'flex-start'
+		};
+
+		$scope.toggleLeftSidebar = function(){
+			$scope.leftSideVisible = $scope.leftSideVisible ? false : true;
+		};
+
+		$scope.toggleRightSidebar = function(){
+			$scope.rightSideVisible = $scope.rightSideVisible ? false : true;
+		};
+
+		$scope.leftSideVisible = false;
+		$scope.rightSideVisible = false;
+
+		$scope.deleteItem = function(){
+			$scope.mainblocks.pop();
+		};
+
+		$scope.pushItem = function(){
+			$scope.mainblocks.push({
+				text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elitllus a pretium quam. Proin olor sit amet, consectetur',
+				shrink: '0',
+				grow: '1',
+				basis: '250px',
+				order: '',
+				alignself: 'flex-start'
+			});
+		};
 
 		$scope.galleryRows = 
 		[[{url: 'http://lorempixel.com/700/700/?1'},
@@ -35,10 +115,16 @@ angular.module('playground', [])
 			console.log(evt);
 		};
 
+		$scope.showModal = false;
+
+		$scope.showAccordion = false;
+		$scope.showMain = true;
+		$scope.showGallery = false;
+
 		$scope.main = {
 			justifying: 'center',
 			wrap: 'wrap',
-			direction: 'column',
+			direction: 'row',
 			alignitems: 'flex-start',
 			aligncontent: 'flex-start'
 		};
@@ -46,6 +132,16 @@ angular.module('playground', [])
 		$scope.addImagesToGallery = function(){
 			this.galleryRows.forEach(function(array){
 				array.push({url: 'http://lorempixel.com/700/700/?' + Math.floor(Math.random()*10) })
+			});
+		};
+
+		$scope.applyGlobal = function(){
+			$scope.mainblocks.forEach(function(item){
+				item.shrink = $scope.global.shrink;
+				item.grow = $scope.global.grow;
+				item.basis = $scope.global.basis;
+				item.order = $scope.global.order;
+				item.alignself = $scope.global.alignself;
 			});
 		};
 
@@ -80,4 +176,54 @@ angular.module('playground', [])
 			document.getElementsByClassName('main-first-block-wrapper')[0].style.alignContent = this.main.aligncontent;
 		};
 
-	}]);
+		$scope.showAccordionFunc = function(){
+			$scope.showAccordion = $scope.showAccordion ? false : true;
+		};
+
+		$scope.showMainFunc = function(){
+			$scope.showMain = $scope.showMain ? false : true;
+		};
+
+		$scope.showGalleryFunc = function(){
+			$scope.showGallery = $scope.showGallery ? false : true;
+		};
+
+		$scope.showCSSCode = function(evt){
+			var i = 0;
+		 	$scope.properties = [];
+		 	$scope.elementClass = evt.target.parentElement.classList[0];
+			while(evt.target.parentElement.style[i]){
+				$scope.properties.push({name:evt.target.parentElement.style[i],
+					value: evt.target.parentElement.style[evt.target.parentElement.style[i]]});
+				i++;
+			}
+			console.log($scope.properties);
+			$scope.showModal = true;
+			setTimeout(function(){
+				$scope.$apply();	
+			}, 1000);
+			
+		};
+
+		$scope.closeModal = function(evt){
+			if (evt.target.classList[0]=='modal') {
+				$scope.showModal = false;
+			}
+		};
+
+	}])
+.directive('cssCode', function(){
+		return {
+			restrict: 'EA',
+			template: ' <div class="modal-wrapper">\
+			<div><span class="row-number">1</span><span class="className">{{elementClass}}</span> {</div>  \
+				 \
+					<div class="properties"> \
+						<div ng-repeat="prop in properties"> \
+							<div class="property-row"><span class="row-number">{{$index+2}}</span><span class="property">{{prop.name}}</span>: <span class="value">{{prop.value}}</span>;</div> \
+						</div> \
+					</div>\
+				<span class="row-number">{{properties.length+2}}</span>} \
+			</div>'
+		};
+	});
